d4094dd37a34f7521a0d33d402a9b35655f3e05a
philipp kern <philippk8@gmail.com>
2022-12-20 21:37:43 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,15 +21,7 @@ test_bike_1 = Bike.create!(name: "Cool e-bike", description: "very nice bike, be
   another one. It was a shame to have this one in my basement without use", model: "e-bike", location: "Berlin", year: 2021, price: 25.99, user_id: klaus.id)
 puts "created #{test_bike_1.id}"
 
-test_bike_2 = Bike.create!(name: "Stevens", description: "These are the tecnical details. The bike is for beginners and
-  very easy to ride.", model: "XPF-8545", location: "Berlin-Spandau", year: 2015, price: 65.99, user_id: peter.id)
-puts "created #{test_bike_2.id}"
-
-test_bike_3 = Bike.create!(name: "Little bike", description: "Bike for children between 5 and 7 years. It is too
-  small for my son. Everything is working and for the price I can offer a helmet on top", model: "childweels", location: "Berlin-Mitte", year: 2020, price: 40, user_id: klaus.id)
-puts "created #{test_bike_3.id}"
-
-booking_1 = Booking.create(start: Date.new(2022, 4, 3), end: Date.new(2022, 4, 6), user_id: peter.id, bike_id: test_bike_3.id)
+booking_1 = Booking.create(start: Date.new(2022, 4, 3), end: Date.new(2022, 4, 6), user_id: peter.id, bike_id: test_bike_1.id)
 puts "created #{booking_1.id}"
 
 32.times do
